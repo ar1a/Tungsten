d3395c4e66a5ed87266278d6743ceb604ab8005c
Aria <aria@ar1as.space>
2018-05-24 15:36:42 +1000

    One wildcard-y boi

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Prisma } from './generated/prisma'
-import resolvers from './resolvers'
+import { GraphQLServer } from 'graphql-yoga';
+import { Prisma } from './generated/prisma';
+import resolvers from './resolvers';
 
+// one recompile-y comment
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
@@ -9,9 +10,9 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      debug: true // log all GraphQL queries & mutations sent to the Prisma API
       // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
-    }),
-  }),
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+    })
+  })
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
