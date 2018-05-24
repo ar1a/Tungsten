f712b488a64491cc5234e80068ac02330d650ab8
Aria <aria@ar1as.space>
2018-05-24 15:28:48 +1000

    Fix those nasty typescript errors
    ugh i should use javascript

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,13 +1,11 @@
-import { Query } from './Query'
-import { auth } from './Mutation/auth'
-import { post } from './Mutation/post'
-import { AuthPayload } from './AuthPayload'
+import { Query } from './Query';
+import { auth } from './Mutation/auth';
+import { AuthPayload } from './AuthPayload';
 
 export default {
   Query,
   Mutation: {
-    ...auth,
-    ...post,
+    ...auth
   },
-  AuthPayload,
-}
+  AuthPayload
+};
