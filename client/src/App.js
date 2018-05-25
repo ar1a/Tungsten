70aecbcc547ce82e81981de45ea63004bbd90c5a
Aria <aria@ar1as.space>
2018-05-25 13:36:34 +1000

    Add sandbox page thingo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import { Spinner, FocusStyleManager } from '@blueprintjs/core';
+import { FocusStyleManager } from '@blueprintjs/core';
+import { query, mutation, Connect } from 'urql';
 
 // http://blueprintjs.com/docs/v2/#core/accessibility
 FocusStyleManager.onlyShowFocusOnTabs();
 
+const HOME_QUERY = `
+{
+  me {
+    email
+    id
+  }
+}
+`;
+
+const LOGIN_MUTATION = mutation(`
+mutation ($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      name
+    }
+  }
+}
+`);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Spinner />
-      </div>
+      <Connect query={query(HOME_QUERY)} mutation={{ login: LOGIN_MUTATION }}>
+        {({ data, loaded, ...rest }) => {
+          console.log(data, loaded, rest);
+          return <div className="App">Hello</div>;
+        }}
+      </Connect>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Client, Provider } from 'urql';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new Client({
+  url: 'http://localhost:4000/'
+});
+
+ReactDOM.render(
+  <Provider client={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
