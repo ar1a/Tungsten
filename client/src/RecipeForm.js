51dd8a5d9e5fe236affa914c292163fd130014ca
Aria <aria@ar1as.space>
2018-05-31 22:30:24 +1000

    Add dynamic text to recipeform create button

--- client/src/RecipeForm.js
+++ client/src/RecipeForm.js
@@ -308,7 +308,7 @@ export default ({
               primary
               raised
             >
-              Create
+              {edit ? 'Update' : 'Create'}
             </Button>
           </Cell>
         </Grid>
